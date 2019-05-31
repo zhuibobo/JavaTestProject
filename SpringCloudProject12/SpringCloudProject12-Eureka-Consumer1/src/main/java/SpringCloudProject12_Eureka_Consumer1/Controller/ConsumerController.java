caa9f3bc131170bef464aa354b15c62ca8b70fe0
zhuibobo <zhuibobo@qq.com>
2019-05-31 11:40:29 +0800

    zhuibobo

--- SpringCloudProject12/SpringCloudProject12-Eureka-Consumer1/src/main/java/SpringCloudProject12_Eureka_Consumer1/Controller/ConsumerController.java
+++ SpringCloudProject12/SpringCloudProject12-Eureka-Consumer1/src/main/java/SpringCloudProject12_Eureka_Consumer1/Controller/ConsumerController.java
@@ -1,6 +1,9 @@
 package SpringCloudProject12_Eureka_Consumer1.Controller;
 
 import SpringCloudProject12_Eureka_Consumer1.WebService.HelloRemote;
+import SpringCloudProject12_Eureka_Consumer1.pojo.Relationship_1to1_cpu;
+import SpringCloudProject12_Eureka_Consumer1.pojo.Relationship_1to1_pc;
+import SpringCloudProject12_Eureka_Consumer1.service.Relationship_1to1_Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +18,22 @@ public class ConsumerController {
 
     @RequestMapping("/hello/{name}")
     public String index(@PathVariable("name") String name) {
+
+        Relationship_1to1_Service relationship_1to1_service=new Relationship_1to1_Service();
+        insertNewPC(relationship_1to1_service);
+
         return HelloRemote.hello(name);
     }
 
+    private Relationship_1to1_pc insertNewPC(Relationship_1to1_Service relationship_1to1_service) {
+        Relationship_1to1_pc relationship_1to1_pc=new Relationship_1to1_pc();
+        relationship_1to1_pc.setId(1);
+        relationship_1to1_pc.setName("我的电脑");
+        Relationship_1to1_cpu relationship_1to1_cpu=new Relationship_1to1_cpu();
+        relationship_1to1_cpu.setId(1);
+        relationship_1to1_cpu.setName("我的CPU");
+        relationship_1to1_pc.setCpu(relationship_1to1_cpu);
+        relationship_1to1_service.insert2table(relationship_1to1_service.getSessionFactory(),relationship_1to1_pc);
+        return relationship_1to1_pc;
+    }
 }
