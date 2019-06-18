6681e7e567837598dec20a152aed1811715dc003
zhuibobo <zhuibobo@qq.com>
2019-06-18 17:55:35 +0800

    zhuibobo

--- SpringCloudProject12/SpringCloudProject12-Config-Client/src/main/java/SpringCloudProject12_Config_Client/Controller/HelloController.java
+++ SpringCloudProject12/SpringCloudProject12-Config-Client/src/main/java/SpringCloudProject12_Config_Client/Controller/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.charset.StandardCharsets;
+
 @RestController
 class HelloController {
     @Value("${neo.hello}")
@@ -11,6 +13,9 @@ class HelloController {
 
     @RequestMapping("/hello")
     public String from() {
-        return this.hello;
+
+        String str = new String(this.hello.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
+
+        return str;
     }
 }
