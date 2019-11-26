edb1649089858cf82dc2f8cb69712767e83c1dd7
zhuibobo <j.d.r565>
2019-11-26 14:47:26 +0800

    测试工程

--- CamundaProject13/src/test/java/CamundaProject13/P004/P004Test.java
+++ CamundaProject13/src/test/java/CamundaProject13/P004/P004Test.java
@@ -2,6 +2,9 @@ package CamundaProject13.P004;
 
 import CamundaProject13.CamundaUtility;
 import CamundaProject13.MainApplicationTest;
+import com.jb4dc.base.tools.XMLUtility;
+import com.jb4dc.workflow.integrate.WorkflowIntegrate;
+import com.jb4dc.workflow.po.bpmn.BpmnDefinitions;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.ProcessEngineConfiguration;
 import org.camunda.bpm.engine.RepositoryService;
@@ -13,9 +16,13 @@ import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
 import org.camunda.bpm.engine.impl.context.Context;
 import org.camunda.bpm.engine.impl.repository.DeploymentBuilderImpl;
 import org.camunda.bpm.engine.repository.DeploymentBuilder;
+import org.junit.Assert;
 import org.junit.Test;
 
+import javax.xml.bind.JAXBException;
+import javax.xml.stream.XMLStreamException;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created with IntelliJ IDEA.
@@ -25,11 +32,21 @@ import java.io.IOException;
  */
 public class P004Test extends MainApplicationTest {
     @Test
-    public void P004_001_bpmn() throws InterruptedException, IOException {
+    public void P004_001_bpmn() throws InterruptedException, IOException, JAXBException, XMLStreamException {
         ProcessEngine processEngine= CamundaUtility.getProcessEngine();
 
-        RepositoryService repositoryService = processEngine.getRepositoryService();
-        repositoryService.createDeployment().name("P004Test-NAME").source("P004Test-Source").tenantId("P004Test-TenantId").addClasspathResource("bpmn/P004_001_发文流程.bpmn").deploy();
+        InputStream is=this.getClass().getClassLoader().getResourceAsStream("bpmn/P004_001_发文流程_解析XML模型用.bpmn");
+
+        //RepositoryService repositoryService = processEngine.getRepositoryService();
+        //repositoryService.createDeployment().name("P004Test-NAME").source("P004Test-Source").tenantId("P004Test-TenantId").addClasspathResource("bpmn/P004_001_发文流程.bpmn").deploy();
+
+        WorkflowIntegrate workflowIntegrate=new WorkflowIntegrate();
+        BpmnDefinitions bpmnDefinitions=workflowIntegrate.parseToPO(is);
+
+        Assert.assertEquals("发文流程",bpmnDefinitions.getBpmnProcess().getName());
+        Assert.assertEquals("发文流程V1",bpmnDefinitions.getBpmnProcess().getVersionTag());
+
+        consolelog(XMLUtility.toXMLString(bpmnDefinitions));
 
         //repositoryService.createDeployment().
         //Context.setProcessEngineConfiguration((ProcessEngineConfigurationImpl) processEngine.getProcessEngineConfiguration());
