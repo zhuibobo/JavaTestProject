71cb70c5f763205df82dee29b7fa95816db9acf7
zhuibobo <j.d.r565>
2019-11-27 15:41:10 +0800

    ElectronTestProject

---
+++ ElectronProject14/src/main/ElectronDemo/chapter-02/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+
+app.on('window-all-closed',() => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('ready', () => {
+  // Extra configuration settings added due to issue, see 
+  // https://github.com/paulbjensen/cross-platform-desktop-applications/issues/9
+  mainWindow = new BrowserWindow({ webPreferences: { nodeIntegration: true } });
+  mainWindow.loadURL(`file://${app.getAppPath()}/index.html`);
+  mainWindow.on('closed', () => { mainWindow = null; });
+  //mainWindow.webContents.openDevTools();
+});
---
+++ ElectronProject14/src/main/ElectronDemo/chapter-03/search.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const lunr = require('lunr');
+let index;
+
+function resetIndex() {
+  index = lunr(function () {
+    this.field('file');
+    this.field('type');
+    this.ref('path');
+  });
+}
+
+function addToIndex(file) {
+  console.log(index);
+  //index.add(file);
+}
+
+function find(query, cb) {
+  if (!index) {
+    resetIndex();
+  }
+
+  const results = index.search(query);
+  cb(results);
+}
+
+module.exports = { addToIndex, find, resetIndex };
