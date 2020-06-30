88479224ef56015a7a84606d020d95ffd581990d
zhuibobo <j.d.r565>
2020-06-30 11:46:30 +0800

    test

--- AndroidApplicationProject18/app/src/main/java/com/example/myapplication18/navigation_drawer/NavigationViewTest002Activity.java
+++ AndroidApplicationProject18/app/src/main/java/com/example/myapplication18/navigation_drawer/NavigationViewTest002Activity.java
@@ -1,22 +1,21 @@
 package com.example.myapplication18.navigation_drawer;
 
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import android.content.res.Configuration;
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import com.example.myapplication18.R;
-import com.google.android.material.navigation.NavigationView;
 
 public class NavigationViewTest002Activity extends AppCompatActivity {
 
@@ -28,14 +27,14 @@ public class NavigationViewTest002Activity extends AppCompatActivity {
         setContentView(R.layout.activity_navigation_view_test002);
 
         DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ListView drawerList = (ListView) findViewById(R.id.left_drawer);
-
         drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+        ListView drawerList = (ListView) findViewById(R.id.left_drawer);
         drawerList.setAdapter(new ArrayAdapter<>(this,
                 R.layout.drawer_list_item,
                 new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"}));
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Toolbar");
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeButtonEnabled(true);
@@ -50,6 +49,38 @@ public class NavigationViewTest002Activity extends AppCompatActivity {
         );
 
         drawerLayout.setDrawerListener(mDrawerToggle);
+
+        Button btn_open_left=findViewById(R.id.btn_open_left);
+        btn_open_left.setOnClickListener((View v)->{
+            drawerLayout.openDrawer(GravityCompat.START);
+        });
+
+        Button btn_open_right=findViewById(R.id.btn_open_right);
+        btn_open_right.setOnClickListener((View v)->{
+            drawerLayout.openDrawer(GravityCompat.END);
+        });
+
+        drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View view, float v) {
+                Log.i("---", "滑动中");
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View view) {
+                Log.i("---", "打开");
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View view) {
+                Log.i("---", "关闭");
+            }
+
+            @Override
+            public void onDrawerStateChanged(int i) {
+                Log.i("---", "状态改变");
+            }
+        });
     }
 
     @Override
--- AndroidApplicationProject18/app/src/main/java/com/example/myapplication18/navigation_drawer/NavigationViewTest003Activity.java
+++ AndroidApplicationProject18/app/src/main/java/com/example/myapplication18/navigation_drawer/NavigationViewTest003Activity.java
@@ -1,16 +1,80 @@
 package com.example.myapplication18.navigation_drawer;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import com.example.myapplication18.MainActivity;
 import com.example.myapplication18.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class NavigationViewTest003Activity extends AppCompatActivity {
-
+    ActionBarDrawerToggle mDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_view_test003);
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Toolbar");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close
+        );
+
+        drawerLayout.setDrawerListener(mDrawerToggle);
+
+        final NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.getHeaderView(0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawer(navigationView);
+                Toast.makeText(NavigationViewTest003Activity.this, "Header View is clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                /*switch (item.getItemId()) {
+                    case R.id.menu_home:
+                        Toast.makeText(MainActivity.this, "Home is clicked!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_settings:
+                        Toast.makeText(MainActivity.this, "Settings is clicked!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_share:
+                        Toast.makeText(MainActivity.this, "Share is clicked!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_about:
+                        Toast.makeText(MainActivity.this, "About is clicked!", Toast.LENGTH_SHORT).show();
+                        break;
+                }*/
+                drawerLayout.closeDrawer(navigationView);
+                return false;
+            }
+        });
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
     }
 }
